212caae6ae360128321bd61d8f467deb4010e8a2
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-30 17:56:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,30 @@
 def game_hash
   teams = Hash.new { |hash, key| hash[key] = Hash.new(&hash.default_proc) }
   
-  teams[:home] = "Brooklyn Nets"
-  teams[:home][:colors] = ["Black", "White"]
+  teams # => {
+  :home => {
+    :western => {
+      :pacific => [
+        [0] "sharks",
+        [1] "oilers"
+      ],
+      :central => [
+        [0] "blues",
+        [1] "stars"
+      ]
+    },
+    :eastern => {
+      :metropolitan => [
+        [0] "penguins",
+        [1] "flyers"
+      ],
+      :atlantic => [
+        [0] "redwings",
+        [1] "bruins"
+      ]
+    }
+  }
+}
 
 
 
