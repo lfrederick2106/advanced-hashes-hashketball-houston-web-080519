0f0db8bb5d68fec0383ce8749ea61a6e2bf5f5ed
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-30 18:11:54 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,14 @@
-# Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+  teams = Hash.new { |hash, key| hash[key] = Hash.new(&hash.default_proc) }
+  
+  teams # => {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black","White"]
+    }
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise","Purple"]
+    }
+  }
+end
